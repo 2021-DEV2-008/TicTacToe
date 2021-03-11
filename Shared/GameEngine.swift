37c66ac82cd8381afe12cd7d1f7dde0ddd59a8c9
Anonymous Candidate <anon@example.com>
2021-03-11 14:47:09 +0100

    GameEngine: createNewGame

---
+++ Shared/GameEngine.swift
@@ -0,0 +1,26 @@
+//
+//  GameEngine.swift
+//  TicTacToe
+//
+//  Created by Jérôme Danthinne on 11/03/2021.
+//
+
+import Foundation
+
+class GameEngine: ObservableObject {
+    enum State: Int {
+        case blank = 0
+        case x = 1
+        case o = 2
+    }
+    
+    @Published private(set) var size: Int = 0
+    @Published private(set) var grid = [[State]]()
+    
+    func startNewGame(ofSize size: Int) {
+        self.size = size
+        grid = Array(repeating: Array(repeating: .blank,
+                                      count: size),
+                     count: size)
+    }
+}
