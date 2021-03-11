7e19158b0d4506c00446e1d68ee6a0ed9c853527
Anonymous Candidate <anon@example.com>
2021-03-11 14:27:41 +0100

    XCode SwiftUI template

---
+++ Shared/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  Shared
+//
+//  Created by Jérôme Danthinne on 11/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
