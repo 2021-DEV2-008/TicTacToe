d139d7c555b7d958eba95356b4d451b6a177460a
Anonymous Candidate <anon@example.com>
2021-03-12 10:05:04 +0100

    GameEngine: check Game end

--- Shared/GameEngine.swift
+++ Shared/GameEngine.swift
@@ -14,8 +14,15 @@ class GameEngine: ObservableObject {
         case o = 2
     }
     
+    enum GameEnd {
+        case winnerX
+        case winnerO
+        case draw
+    }
+    
     @Published private(set) var size: Int = 3
     @Published private(set) var grid = [[State]]()
+    @Published private(set) var gameEnd: GameEnd?
     private(set) var currentPlayer: State = .x
     private(set) var numberOfMoves: Int = 0
     
@@ -24,7 +31,7 @@ class GameEngine: ObservableObject {
     }
     
     func startNewGame(ofSize size: Int) {
-        self.objectWillChange.send()
+        objectWillChange.send()
         
         self.size = size
         grid = Array(repeating: Array(repeating: .blank,
@@ -32,6 +39,7 @@ class GameEngine: ObservableObject {
                      count: size)
         currentPlayer = .x
         numberOfMoves = 0
+        gameEnd = nil
     }
     
     func makeMove(x: Int, y: Int, state: State) {
@@ -45,9 +53,71 @@ class GameEngine: ObservableObject {
         
         // Increment the number of moves
         numberOfMoves += 1
+        
+        // Check end of game.
+        if let gameEnd = checkGameEnd(withLastMoveAtX: x, y: y) {
+            self.gameEnd = gameEnd
+        } else {
+            newTurn()
+        }
     }
     
     func newTurn() {
         currentPlayer = currentPlayer == .x ? .o : .x
+    }
+    
+    func checkGameEnd(withLastMoveAtX x: Int, y: Int) -> GameEnd?  {
+        // Check played row.
+        let row = grid[x]
+        if let winningRow = checkWinner(in: row) {
+            return winningRow
+        }
+
+        // Check played line.
+        let line = grid.map { $0[y] }
+        if let winningLine = checkWinner(in: line) {
+            return winningLine
+        }
+
+        // If on the diagonal, check it.
+        if x == y {
+            let diagonal = grid.enumerated().map { $0.element[$0.offset] }
+            if let winningDiagonal = checkWinner(in: diagonal) {
+                return winningDiagonal
+            }
+        }
+
+        // If on the inverse diagonal, check it.
+        if x + 1 == size - y {
+            let inverseDiagonal = grid.enumerated().map { $0.element[size - 1 - $0.offset] }
+            if let winningDiagonal = checkWinner(in: inverseDiagonal) {
+                return winningDiagonal
+            }
+        }
+
+        // If all positions have been played, it's a draw.
+        if numberOfMoves == size * size {
+            return .draw
+        }
+
+        return nil
+    }
+    
+    private func checkWinner(in items: [State]) -> GameEnd? {
+        // If items not fully played, no winner.
+        guard !items.contains(.blank) else { return nil }
+
+        let sum = items.reduce(0, { $0 + $1.rawValue })
+
+        if sum == State.x.rawValue * size {
+            // Full of X's
+            return .winnerX
+        } else if sum == State.o.rawValue * size {
+            // Full of O's
+            return .winnerO
+        } else {
+            // Mix of O's and X's
+            return nil
+        }
     }
 }
--- Tests iOS/Tests_iOS.swift
+++ Tests iOS/Tests_iOS.swift
@@ -29,6 +29,9 @@ class Tests_iOS: XCTestCase {
         
         // Number of moves should be set back to 0.
         XCTAssertEqual(gameEngine.numberOfMoves, 0)
+        
+        // No winner yet.
+        XCTAssertNil(gameEngine.gameEnd)
     }
     
     func testNewTurn() throws {
@@ -57,5 +60,83 @@ class Tests_iOS: XCTestCase {
         // The position should still be the first played state, and number of moves not incremented.
         XCTAssertEqual(gameEngine.grid[0][0], .x)
         XCTAssertEqual(gameEngine.numberOfMoves, 1)
+    }
+    
+    func testEndOfGameWithRowFullOfXs() throws {
+        gameEngine.makeMove(x: 0, y: 0, state: .x)
+        XCTAssertNil(gameEngine.gameEnd)
+        gameEngine.makeMove(x: 1, y: 0, state: .x)
+        XCTAssertNil(gameEngine.gameEnd)
+        gameEngine.makeMove(x: 2, y: 0, state: .x)
+        
+        // Row is full of X's, winner should be X.
+        XCTAssertEqual(gameEngine.gameEnd, .winnerX)
+    }
+    
+    func testEndOfGameWithLineFullOfOs() throws {
+        gameEngine.makeMove(x: 0, y: 0, state: .o)
+        XCTAssertNil(gameEngine.gameEnd)
+        gameEngine.makeMove(x: 0, y: 1, state: .o)
+        XCTAssertNil(gameEngine.gameEnd)
+        gameEngine.makeMove(x: 0, y: 2, state: .o)
+        
+        // Line is full of O's, winner should be O.
+        XCTAssertEqual(gameEngine.gameEnd, .winnerO)
+    }
+    
+    func testEndOfGameWithWinningDiagonal() throws {
+        gameEngine.makeMove(x: 0, y: 0, state: .x)
+        XCTAssertNil(gameEngine.gameEnd)
+        gameEngine.makeMove(x: 1, y: 1, state: .x)
+        XCTAssertNil(gameEngine.gameEnd)
+        gameEngine.makeMove(x: 2, y: 2, state: .x)
+        
+        // Line is mixed, game is not ended.
+        XCTAssertEqual(gameEngine.gameEnd, .winnerX)
+    }
+    
+    func testEndOfGameWithWinningInverseDiagonal() throws {
+        gameEngine.makeMove(x: 0, y: 2, state: .o)
+        XCTAssertNil(gameEngine.gameEnd)
+        gameEngine.makeMove(x: 1, y: 1, state: .o)
+        XCTAssertNil(gameEngine.gameEnd)
+        gameEngine.makeMove(x: 2, y: 0, state: .o)
+        
+        // Line is mixed, game is not ended.
+        XCTAssertEqual(gameEngine.gameEnd, .winnerO)
+    }
+    
+    func testEndOfGameWithMixedRow() throws {
+        gameEngine.makeMove(x: 0, y: 0, state: .x)
+        XCTAssertNil(gameEngine.gameEnd)
+        gameEngine.makeMove(x: 1, y: 0, state: .o)
+        XCTAssertNil(gameEngine.gameEnd)
+        gameEngine.makeMove(x: 2, y: 0, state: .x)
+        
+        // Line is mixed, game is not ended.
+        XCTAssertNil(gameEngine.gameEnd)
+    }
+
+    func testEndOfGameWithNoWinner() throws {
+        gameEngine.makeMove(x: 0, y: 0, state: .x)
+        XCTAssertNil(gameEngine.gameEnd)
+        gameEngine.makeMove(x: 1, y: 0, state: .o)
+        XCTAssertNil(gameEngine.gameEnd)
+        gameEngine.makeMove(x: 2, y: 0, state: .x)
+        XCTAssertNil(gameEngine.gameEnd)
+        gameEngine.makeMove(x: 0, y: 1, state: .x)
+        XCTAssertNil(gameEngine.gameEnd)
+        gameEngine.makeMove(x: 1, y: 1, state: .o)
+        XCTAssertNil(gameEngine.gameEnd)
+        gameEngine.makeMove(x: 2, y: 1, state: .x)
+        XCTAssertNil(gameEngine.gameEnd)
+        gameEngine.makeMove(x: 0, y: 2, state: .o)
+        XCTAssertNil(gameEngine.gameEnd)
+        gameEngine.makeMove(x: 1, y: 2, state: .x)
+        XCTAssertNil(gameEngine.gameEnd)
+        gameEngine.makeMove(x: 2, y: 2, state: .o)
+        
+        // All positions played.
+        XCTAssertEqual(gameEngine.gameEnd, .draw)
     }
 }
